736d22d08422a0e5c905473b8dfa54217d1c5a5b
Andrei Savu <savu.andrei@gmail.com>
2024-04-17 15:06:58 -0700

    Getting the example Python API ready for pushdowns for virtual tables

--- python-example-api/api.py
+++ python-example-api/api.py
@@ -4,6 +4,9 @@ import json
 import base64
 import logging
 
+from itertools import permutations
+from inspect import signature
+
 from bottle import Bottle, route, run, response, request
 from bottle_cors_plugin import cors_plugin
 
@@ -12,6 +15,11 @@ app.install(cors_plugin('*'))
 
 CSV_DIRECTORY = 'data'
 MAX_SPLIT_SIZE = 5
+
+FUNCTIONS_SCHEMA_NAME = 'virtual'
+FUNCTIONS = {
+    'permutations': lambda *, word: [''.join(p) for p in permutations(word)]
+}
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,32 +33,60 @@ def read_csv_file(file_path):
 @app.route('/schemas', method='GET')
 def list_schemas():
     schemas = [name for name in os.listdir(CSV_DIRECTORY) if os.path.isdir(os.path.join(CSV_DIRECTORY, name))]
+    schemas.append(FUNCTIONS_SCHEMA_NAME) # tables mapping back into function calls
     response.content_type = 'application/json'
     return json.dumps(schemas)
 
 @app.route('/schemas/<schema>/tables', method='GET')
 def list_tables(schema):
-    schema_path = os.path.join(CSV_DIRECTORY, schema)
-    tables = [{'schema': schema, 'table': name.split('.')[0]} for name in os.listdir(schema_path) if name.endswith('.csv')]
-    response.content_type = 'application/json'
-    return json.dumps(tables)
+    if schema == FUNCTIONS_SCHEMA_NAME:
+        response.content_type = 'application/json'
+        tables = []
+        for function_name in FUNCTIONS:
+            tables.append({'schema': schema, 'table': function_name})
+        return json.dumps(tables)
+    else:
+        schema_path = os.path.join(CSV_DIRECTORY, schema)
+        tables = [{'schema': schema, 'table': name.split('.')[0]} for name in os.listdir(schema_path) if name.endswith('.csv')]
+        response.content_type = 'application/json'
+        return json.dumps(tables)
 
 @app.route('/schemas/<schema>/tables/<table>', method='GET')
 def get_table_metadata(schema, table):
-    file_path = os.path.join(CSV_DIRECTORY, schema, f'{table}.csv')
-    header, _ = read_csv_file(file_path)
-    columns = [{'name': column, 'type': 'varchar', 'comment': None, 'hidden': False} for column in header]
-    metadata = {
-        'schemaTableName': {'schema': schema, 'table': table},
-        'columns': columns,
-        'comment': None,
-    }
+    if schema == FUNCTIONS_SCHEMA_NAME:
+        function = FUNCTIONS.get(table)
+        if function is None:
+            response.status = 404
+            return
+
+        signature_params = list(signature(function).parameters.keys())
+        columns = [{'name': param, 'type': 'varchar'} for param in signature_params]
+        columns.append({'name': 'result', 'type': 'varchar'})
+        metadata = {
+            'schemaTableName': {'schema': schema, 'table': table},
+            'columns': columns,
+            'comment': None,
+        }
+    else:
+        file_path = os.path.join(CSV_DIRECTORY, schema, f'{table}.csv')
+        header, _ = read_csv_file(file_path)
+        columns = [{'name': column, 'type': 'varchar', 'comment': None, 'hidden': False} for column in header]
+        metadata = {
+            'schemaTableName': {'schema': schema, 'table': table},
+            'columns': columns,
+            'comment': None,
+        }
     response.content_type = 'application/json'
     return json.dumps(metadata)
 
 @app.route('/schemas/<schema>/tables/<table>/splits', method='POST')
 def get_splits(schema, table):
     logging.info(f'Splits request: {request.json}')
+
+    # All virtual tables have only one split
+    if schema == FUNCTIONS_SCHEMA_NAME:
+        response.content_type = 'application/json'
+        return json.dumps({'splits': ['0']})
 
     file_path = os.path.join(CSV_DIRECTORY, schema, f'{table}.csv')
     _, data = read_csv_file(file_path)
@@ -66,21 +102,22 @@ def get_splits(schema, table):
     response.content_type = 'application/json'
     return json.dumps(split_batch)
 
-@app.route('/schemas/<schema>/tables/<table>/splits/<split_id>/rows', method='POST')
-def get_rows(schema, table, split_id):
-    logging.info(f'Rows request: {request.json}')
+def construct_column_block(column_data):
+    return {
+        'varcharData': {
+            'nulls': [False] * len(column_data),
+            'sizes': [len(value) for value in column_data],
+            'bytes': base64.b64encode(''.join(column_data).encode('utf-8')).decode('utf-8')
+        }
+    }
 
-    file_path = os.path.join(CSV_DIRECTORY, schema, f'{table}.csv')
-    header, data = read_csv_file(file_path)
-
-    desiredColumns = request.json.get('desiredColumns', None)
-    if desiredColumns is not None:
-        column_indices = [header.index(column) for column in desiredColumns]
+def get_column_indices(header, desired_columns):
+    if desired_columns is not None:
+        return [header.index(column) for column in desired_columns]
     else:
-        # Return all columns in the order they are set in the file
-        column_indices = list(range(len(header)))
+        return list(range(len(header)))
 
-    next_token = request.json.get('nextToken')
+def get_rows_range(split_id, next_token, data):
     if next_token:
         start, end = map(int, next_token.split('-'))
     else:
@@ -94,16 +131,62 @@ def get_rows(schema, table, split_id):
         rows = data[start:mid]
         next_token = f"{mid}-{end}"
 
+    return rows, next_token
+
+def handle_function_call(table, request_json):
+    function = FUNCTIONS.get(table)
+    if function is None:
+        response.status = 404
+        return
+
+    method_kwargs_names = list(signature(function).parameters.keys())
+    desired_columns = request_json.get('desiredColumns')
+    assert desired_columns == method_kwargs_names + ['result']
+
+    # get the method parameters and call the function
+    method_kwargs = {'word': 'rocket', }  # {param: request_json[param] for param in method_params}
+    result = function(**method_kwargs)
+    row_count = len(result)
+
+    column_blocks = []
+    for column in desired_columns:
+        if column in method_kwargs_names:
+            column_data = [method_kwargs[column]] * row_count
+        elif column == 'result':
+            column_data = result
+        else:
+            raise ValueError(f'Unknown column: {column}')
+        column_block = construct_column_block(column_data)
+        column_blocks.append(column_block)
+
+    response.content_type = 'application/json'
+    return json.dumps({'columnBlocks': column_blocks, 'rowCount': row_count})
+
+@app.route('/schemas/<schema>/tables/<table>/splits/<split_id>/rows', method='POST')
+def get_rows(schema, table, split_id):
+    logging.info(f'Rows request: {request.json}')
+
+    if schema == FUNCTIONS_SCHEMA_NAME:
+        result = handle_function_call(table, request.json)
+        if result is not None:
+            return result
+        else:
+            return
+
+    file_path = os.path.join(CSV_DIRECTORY, schema, f'{table}.csv')
+    header, data = read_csv_file(file_path)
+
+    desired_columns = request.json.get('desiredColumns', None)
+    column_indices = get_column_indices(header, desired_columns)
+
+    next_token = request.json.get('nextToken')
+    rows, next_token = get_rows_range(split_id, next_token, data)
+
     column_blocks = []
     for i in column_indices:
         column_data = [row[i] for row in rows]
-        column_blocks.append({
-            'varcharData': {
-                'nulls': [False] * len(column_data),
-                'sizes': [len(value) for value in column_data],
-                'bytes': base64.b64encode(''.join(column_data).encode('utf-8')).decode('utf-8')
-            }
-        })
+        column_block = construct_column_block(column_data)
+        column_blocks.append(column_block)
 
     page_result = {'columnBlocks': column_blocks, 'rowCount': len(rows), 'nextToken': next_token}
     response.content_type = 'application/json'
